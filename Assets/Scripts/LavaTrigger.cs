7c0b9297e2f6041d303cf79c6ef23f7bd4344206
RCOST123 <rcostello@conncoll.edu>
2025-11-17 21:26:47 -0500

    level2wjumpissues

--- Assets/Scripts/LavaTrigger.cs
+++ Assets/Scripts/LavaTrigger.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class LavaTrigger : MonoBehaviour
 {
+    public AudioClip lavaSound;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             LavaManager.Instance.StartLavaRise();
+            AudioSource.PlayClipAtPoint(lavaSound, transform.position);
             Destroy(gameObject); // trigger once
         }
     }
--- Assets/Scripts/SteamLift.cs
+++ Assets/Scripts/SteamLift.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class SteamLift : MonoBehaviour
 {
     public float liftForce = 15f; // tweak until it feels right
+    public AudioClip windSound;
 
     private void OnTriggerStay2D(Collider2D other)
     {
@@ -12,6 +13,7 @@ public class SteamLift : MonoBehaviour
             if (rb != null)
             {
                 rb.AddForce(Vector2.up * liftForce * Time.deltaTime, ForceMode2D.Impulse);
+                AudioSource.PlayClipAtPoint(windSound, transform.position);
             }
         }
     }
