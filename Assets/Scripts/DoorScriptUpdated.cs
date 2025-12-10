d1084de071dcb701862f874b53a094f4e19cdeb9
RCOST123 <rcostello@conncoll.edu>
2025-12-10 18:30:37 -0500

    sounds10

--- Assets/Scripts/DoorScriptUpdated.cs
+++ Assets/Scripts/DoorScriptUpdated.cs
@@ -4,13 +4,13 @@ public class DoorScript : MonoBehaviour
 {
     public GameObject nextdoor;
     public Vector3 doorpos;
-    public AudioClip opendoorSound;
+    public AudioSource opendoorSound;
     
     ///public bool door_on = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+     opendoorSound = GameObject.Find("Door_Sound").GetComponent<AudioSource>();   
     }
 
     // Update is called once per frame
@@ -23,7 +23,8 @@ public class DoorScript : MonoBehaviour
         ///doorpos = nextdoor.transform.position;
        if (collision.gameObject.CompareTag("Door"))
        {
-            AudioSource.PlayClipAtPoint(opendoorSound, transform.position);
+           opendoorSound.Play();
+           // AudioSource.PlayClipAtPoint(opendoorSound, transform.position);
             transform.position = doorpos;
        }
     }
